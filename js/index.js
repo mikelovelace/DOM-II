f71be70ecef129fb7e43e63fcb7f510c723cf14e
mikelovelace <mike@mlovelace.net>
2021-03-11 20:37:21 -0500

    added click, dblclick, mouseover events

--- js/index.js
+++ js/index.js
@@ -1,1 +1,24 @@
-// Your code goes here
+// NAV LINKS
+const navLink = document.querySelectorAll(".nav-link")
+
+navLink.forEach(link => {
+    link.addEventListener("click", () => {
+        link.style.color = "white"
+        navBar.style.background = "teal"
+    })
+})
+
+// NAV BAR
+const navBar = document.querySelector(".nav-container")
+
+navBar.addEventListener("dblclick", () => {
+    navBar.style.background = "blue"
+    logo.style.color = "white"
+})
+
+// LOGO
+const logo = document.querySelector(".logo-heading")
+
+logo.addEventListener("mouseover", () => {
+    logo.textContent = "Unfun Bus"
+})
